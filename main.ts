1750da58e36cf25f7fa176bd94e077177e9ced9a
programacionuy <157076452+programacion-uruguay@users.noreply.github.com>
2025-03-02 16:56:53 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+function semaforo () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(2000)
+}
+basic.forever(function () {
+    semaforo()
+})
